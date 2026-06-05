86739d2b8f80c698d7409f40595c6383fdef6093
nullture <ysaandre.mendoza@gmail.com>
2026-06-05 15:13:24 +0800

    feat: initialize MERN setup, migrate pages, add navigation and navbar
    feature: fix gitignore, add App.tsx navigation and navbar

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/ysa-about-page";
+import Contact from "./pages/contact-page";
+import Pricing from "./pages/nick-price-page";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
