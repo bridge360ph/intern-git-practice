f79a8f6bdf48c5a0c85609c7dde9d09a73b8042e
raseryu <155956740+raseryu@users.noreply.github.com>
2026-06-09 17:29:38 +0800

    "refactor: rename frontend-new into frontend"

--- frontend-new/src/pages/alfred-ask-us.tsx
+++
@@ -1,8 +0,0 @@
-export default function AskUs() {
-  return (
-    <div>
-      <h1>Ask Us</h1>
-      <p>Your ask us page content here.</p>
-    </div>
-  )
-}
--- frontend-new/src/pages/andrei-contact-us-page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactUs() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>Your contact us page content here.</p>
-    </div>
-  )
-}
